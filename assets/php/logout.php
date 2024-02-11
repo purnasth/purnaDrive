ba63c93a485f937ab2bd3c688d4358dcad60e0ec
Purna Shrestha <crush9918@gmail.com>
2024-02-11 11:08:35 +0545

    gitignore

--- assets/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    header("Location:/form.php");
-    $username = $_SESSION['username'];
-?>
--- assets/php/session.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start();
-?>
-<!-- <label>This is the Session page of Purna Drive.</label> -->
